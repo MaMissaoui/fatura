23f995e86d8cefe989ac718d6467c8fbcce5d5f8
Mohamed Ali Missaoui <mam@missaoui.org>
2026-05-30 10:53:52 +0200

    feat: expose decimal places field on new organization form
    Adds the minimum_fraction_digits input alongside country and currency
    so it can be set at creation time instead of only via settings.
    Defaults to 2.

--- src/routes/organizations/new.tsx
+++ src/routes/organizations/new.tsx
@@ -1,4 +1,4 @@
-import { Form, Input, Select, Typography, Row, Col, Button, Card } from "antd";
+import { Form, Input, InputNumber, Select, Typography, Row, Col, Button, Card } from "antd";
 import { CloseOutlined } from "@ant-design/icons";
 import { atom, useAtom, useSetAtom, useAtomValue } from "jotai";
 import { useNavigate } from "react-router";
@@ -51,14 +51,20 @@ const NewOrganization = () => {
             <Title level={3} style={{ margin: 0 }}>
               <Trans>New Organization</Trans>
             </Title>
-            <Form form={form} layout="vertical" onFinish={handleSubmit} style={{ marginTop: 24 }}>
+            <Form
+              form={form}
+              layout="vertical"
+              onFinish={handleSubmit}
+              style={{ marginTop: 24 }}
+              initialValues={{ minimum_fraction_digits: 2 }}
+            >
               <Form.Item name="name" rules={[{ required: true, message: t`Please input name!` }]}>
                 <Input placeholder={t`Name`} />
               </Form.Item>
               <Row gutter={16}>
-                <Col span={16}>
-                  <Form.Item name="country">
-                    <Select placeholder={t`Country`} showSearch>
+                <Col span={12}>
+                  <Form.Item name="country" label={t`Country`}>
+                    <Select showSearch>
                       {countries.map((country) => (
                         <Select.Option key={country.name} value={country.name}>
                           {country.name}
@@ -67,15 +73,20 @@ const NewOrganization = () => {
                     </Select>
                   </Form.Item>
                 </Col>
-                <Col span={8}>
-                  <Form.Item name="currency">
-                    <Select placeholder={t`Currency`} showSearch>
+                <Col span={6}>
+                  <Form.Item name="currency" label={t`Currency`}>
+                    <Select showSearch>
                       {currencies.map((currency) => (
                         <Select.Option key={currency} value={currency}>
                           {currency}
                         </Select.Option>
                       ))}
                     </Select>
+                  </Form.Item>
+                </Col>
+                <Col span={6}>
+                  <Form.Item name="minimum_fraction_digits" label={t`Decimal places`}>
+                    <InputNumber min={0} max={10} style={{ width: "100%" }} />
                   </Form.Item>
                 </Col>
               </Row>
